8b40d74485d1534daace52a3c7448b8a0341ac65
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-27 11:41:20 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// write code here
+const employees = {
+  john: { salary: 1000, address: { district: 'Ratchathewi', province: 'Bangkok' } },
+  peter: { salary: 1500, address: { district: 'Pathumwan', province: 'Bangkok' } },
+  mike: { salary: 800, address: { district: 'Pakkret', province: 'Nonthaburi' } },
+  sarah: { salary: 2200, address: { district: 'Sriraja', province: 'Chonburi' } }
+};
+function findEmployee() {
+let name = prompt(`Whose info you'd like to know?`)
+if(name in employees){
+  let employee = employees[name]
+    console.log(`Name: ${name}, salary: ${employee.salary}, address: ${employee.address.district}, ${employee.address.province}`)
+  } else {
+    console.log("Not Found")
+  }
+}
